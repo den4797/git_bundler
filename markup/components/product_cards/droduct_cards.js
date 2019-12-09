2b9aed513cb9da0b7f78daa2517fd36be09e19b9
Denis Fishkind <dfi@webcrt.ru>
2019-12-09 13:56:48 +0500

    add elements

---
+++ markup/components/product_cards/droduct_cards.js
@@ -0,0 +1,23 @@
+const product_cards = [
+    {
+        id: "1",
+        name: "Jacket",
+        img: "",
+        price: 40,
+    },
+    {
+        id: "2",
+        name: "Jacket",
+        img: "",
+        price: 70,
+    },
+    {
+        id: "3",
+        name: "Jacket",
+        img: "",
+        price: 60,
+    },
+];
+
+
+export {poduct_cards};
